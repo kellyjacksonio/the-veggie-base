c0b00638dacba2e2965fe81d47d527a47ee26f7e
Kelly Jackson <hi@kellyjackson.io>
2020-11-21 16:24:25 -0600

    Recipe fragment

--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -1,43 +1,37 @@
 import { gql } from "@apollo/client";
 
+const RECIPE_FRAGMENT = gql`
+  fragment RecipeFragment on Recipe {
+    id
+    cookingMethod
+    description
+    ingredients
+    insertedAt
+    instructions
+    name
+    prepTime
+    yields
+    user {
+      id
+      username
+    }
+  }
+`;
+
 export const RECIPES_QUERY = gql`
   query RecipesPageQuery($username: String) {
     recipes(username: $username) {
-      id
-      cookingMethod
-      description
-      ingredients
-      insertedAt
-      instructions
-      name
-      prepTime
-      yields
-      user {
-        id
-        username
-      }
+      ...RecipeFragment
     }
   }
+  ${RECIPE_FRAGMENT}
 `;
 
 export const RECIPE_QUERY = gql`
   query RecipePageQuery($recipeId: String!) {
     recipe(id: $recipeId) {
-      id
-      cookingMethod
-      description
-      ingredients
-      insertedAt
-      instructions
-      name
-      prepTime
-      yields
-      user {
-        id
-        username
-      }
+      ...RecipeFragment
     }
   }
+  ${RECIPE_FRAGMENT}
 `;
-
-// recipe fragment
